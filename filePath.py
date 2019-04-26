94214deb6c4ffb6a53997ce33a35126fcb6336b2
tdanbo <46909851+tdanbo@users.noreply.github.com>
2019-04-26 23:20:16 +1300

    Create filePath.py

---
+++ filePath.py
@@ -0,0 +1,6 @@
+# - dataset - #
+scriptpath = os.path.split((os.path.abspath(__file__)))[0]
+
+dataset = os.path.join(scriptpath,"dataset")
+icons = os.path.join(scriptpath,"icons")
+scan = os.path.join(scriptpath,"scan")
